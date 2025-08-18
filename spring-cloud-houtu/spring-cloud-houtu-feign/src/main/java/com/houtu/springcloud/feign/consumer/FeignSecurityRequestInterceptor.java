4eee070327106348b2b079032939d49c1f2ccafd
jonlu <lujiafayx@163.com>
2025-08-18 17:22:30 +0800

    优化指标监控问题

--- spring-cloud-houtu/spring-cloud-houtu-feign/src/main/java/com/houtu/springcloud/feign/consumer/FeignSecurityRequestInterceptor.java
+++ spring-cloud-houtu/spring-cloud-houtu-feign/src/main/java/com/houtu/springcloud/feign/consumer/FeignSecurityRequestInterceptor.java
@@ -1,24 +1,21 @@
-package com.houtu.springcloud.feign.consumer;
-
-import com.houtu.springcloud.feign.constant.FeignConstant;
-import com.houtu.springcloud.feign.consumer.prop.FeignConsumerProperties;
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
-import jakarta.annotation.Nonnull;
-
-/**
- * Feign异常记录器
- * @author jonlu
- * @date 2022/9/23
- */
-public class FeignSecurityRequestInterceptor implements RequestInterceptor {
-
-
-    public FeignSecurityRequestInterceptor() {
-    }
-
-    @Override
-    public void apply(RequestTemplate requestTemplate) {
-        requestTemplate.request();
-    }
-}
+//package com.houtu.springcloud.feign.consumer;
+//
+//import feign.RequestInterceptor;
+//import feign.RequestTemplate;
+//
+///**
+// * Feign异常记录器
+// * @author jonlu
+// * @date 2022/9/23
+// */
+//public class FeignSecurityRequestInterceptor implements RequestInterceptor {
+//
+//
+//    public FeignSecurityRequestInterceptor() {
+//    }
+//
+//    @Override
+//    public void apply(RequestTemplate requestTemplate) {
+//        requestTemplate.request();
+//    }
+//}
